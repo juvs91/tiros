3096bfc58c4e06a04b3af39b3ce6c26a0b33a9e5
juve <juve-guz@gmail.com>
2013-03-17 19:49:02 -0600

    intent startActivity

--- ShootGoal/src/com/example/shootgoal/ControladorTirador.java
+++ ShootGoal/src/com/example/shootgoal/ControladorTirador.java
@@ -1,5 +1,44 @@
 package com.example.shootgoal;
 
-public class ControladorTirador {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MotionEvent;
 
+public class ControladorTirador extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	
+	
+	
+	/*private float x;
+	private float y;
+	
+	public ControladorTirador(){
+		this(0,0);
+	}
+	public ControladorTirador(float x,float y){
+		this.x=x;
+		this.y=y;
+	}
+	public boolean onTouchEvent(MotionEvent e){
+		x=e.getX();
+		y=e.getY();
+		
+		
+		return true;
+		
+	}*/
 }
--- ShootGoal/src/com/example/shootgoal/MainActivity.java
+++ ShootGoal/src/com/example/shootgoal/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.shootgoal;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
-
-public class MainActivity extends Activity {
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+public class MainActivity extends Activity implements OnClickListener {
+	int vista=0;//vista inicial 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.pr);
+		
+		Button boton= ((Button) findViewById(R.id.button1));
+		boton.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +24,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+        		Intent launchGame=new Intent(this,ControladorTirador.class);
+        		startActivity(launchGame);	
+		
 	}
 
 }
