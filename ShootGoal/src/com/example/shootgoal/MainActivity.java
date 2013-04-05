9815547628c276dbed7ee41a2e037523adb6a77e
David Tovar Goris <sgoris@gmail.com>
2013-04-04 21:48:42 -0600

    Lala cambios al Main y a sus layouts

--- ShootGoal/src/com/example/shootgoal/MainActivity.java
+++ ShootGoal/src/com/example/shootgoal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shootgoal;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,22 +10,25 @@ import android.view.WindowManager;
 import android.widget.ImageButton;
 public class MainActivity extends Activity implements OnClickListener {
 	int vista=0;//vista inicial 
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pr);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		
-		ImageButton boton= ((ImageButton) findViewById(R.id.button1));
+
+		ImageButton boton= ((ImageButton) findViewById(R.id.butPlay));
 		boton.setOnClickListener(this);
+		
+
 	}
 
 
 	@Override
 	public void onClick(View arg0) {
-        		Intent launchGame=new Intent(this,ControladorTirador.class);
-        		startActivity(launchGame);	
+		Intent launchGame=new Intent(this,ControladorTirador.class);
+		startActivity(launchGame);	
+
 	}
 
 }
